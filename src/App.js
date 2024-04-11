3c9a1c92921063f439d0d2b1de56354b00850df3
Everton Petean <evertonpetean@gmail.com>
2024-04-11 11:02:51 -0300

    Prev_prox_dias

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   //chamada da API
   const handleSearch = () => {
     if (city) {
-      fetch(`https://api.weatherapi.com/v1/forecast.json?key=cfc844e87500429f992194636233010&q=${city}&lang=pt`)
+      fetch(`https://api.weatherapi.com/v1/forecast.json?key=cfc844e87500429f992194636233010&q=${city}&days=7&lang=pt`)
         .then((response) => {
           if (!response.ok) {
             throw new Error('Erro na solicitação');
@@ -42,10 +42,10 @@ function App() {
       </nav>
       <main class="container">
         <div class='jumbotron'>
-          <h1>
-            Verifique agora a previsão do tempo em sua cidade!
-          </h1>
-          <p class='lead'>
+          <h2>
+            Verifique a previsão do tempo em sua cidade!
+          </h2>
+          <p class='lead '>
             Digite o nome da sua cidade no campo abaixo e em seguida clique em pesquisar.
           </p>
           <p class=''>
@@ -65,18 +65,51 @@ function App() {
 
           {error && <p class="text-danger mt-1">{error}</p>}
           {weatherData && (
-            <div class='weather-data mt-4 d-flex flex-column align-items-center' >
-              <p class='h4'>Agora em {weatherData.location.name}, {weatherData.location.region} - {weatherData.location.country}</p>
-              <span class='mt-3 mb-0'><img src={weatherData.current.condition.icon} alt='' /></span>
-              <div class='lead d-flex flex-column font-weight-bold font-italic '>
-                <span class='h1'>{weatherData.current.temp_c} °C  </span>
-                <span class=''>{weatherData.current.condition.text} </span>
-                <span class=''>Umidade do ar: {weatherData.current.humidity}%</span>
-                <span class=''>Vento: {weatherData.current.wind_kph} km/h </span>
+            <div class='weather-data mt-4 d-flex flex-column ' >
+              <div class='current d-flex flex-column align-items-center'>
+                <p class='h4'>Agora em {weatherData.location.name}, {weatherData.location.region} - {weatherData.location.country}</p>
+                <span class='mt-1 mb-0'>
+                  <img src={weatherData.current.condition.icon} alt='' />
+                  <span class='h4'>{weatherData.current.temp_c} °C </span>
+                </span>
+                <div class='lead d-flex flex-column font-weight-bold font-italic '>
+                  <p class='h6 font-weight-bold'>Umidade do ar: {weatherData.current.humidity}%</p>
+                  <p class='h6 font-weight-bold'>Vento: {weatherData.current.wind_dir} - {weatherData.current.wind_kph} km/h </p>
+                </div>
               </div>
+              <div class="row">
+                <div class="col-sm-6 mt-4">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Previsão Hoje </h5>
+                      <img src={weatherData.forecast.forecastday[0].day.condition.icon} alt='' />
+                      <p class="card-text">Máxima: {weatherData.forecast.forecastday[0].day.maxtemp_c} °C </p>
+                      <p class="card-text">Minima: {weatherData.forecast.forecastday[0].day.mintemp_c} °C</p>
+                      <p class="card-text">Condição: {weatherData.forecast.forecastday[0].day.condition.text}. </p>
+                      <p class="card-text">Chuva: {weatherData.forecast.forecastday[0].day.daily_chance_of_rain}% - {weatherData.forecast.forecastday[0].day.totalprecip_mm}mm </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6 mt-4">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Previsão Amanhã</h5>
+                      <img src={weatherData.forecast.forecastday[1].day.condition.icon} alt='' />
+                      <p class="card-text">Máxima: {weatherData.forecast.forecastday[1].day.maxtemp_c} °C </p>
+                      <p class="card-text">Minima: {weatherData.forecast.forecastday[1].day.mintemp_c} °C</p>
+                      <p class="card-text">Condição: {weatherData.forecast.forecastday[1].day.condition.text}. </p>
+                      <p class="card-text">Chuva: {weatherData.forecast.forecastday[1].day.daily_chance_of_rain}% - {weatherData.forecast.forecastday[0].day.totalprecip_mm}mm </p>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
             </div>
 
+
           )}
+
 
 
         </div>
